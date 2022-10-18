65671de0694424b606e4b14b8399c5c6b82b673e
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2022-10-18 19:51:56 +0900

    Feat: pptx parser 유틸 추가 1. pptx 파일을 읽어 view data로 추출해주는 유틸 함수를 생성하였습니다. 2. 해당 유틸 사용위해, jszip 라이브러리 사용하였습니다. 3. slide의 기본 크기/각 slide별 id/slide 내 요소들에 대한 정보를 가져오는 로직입니다. 4. 요소는 현재 image와 text를 지원하는 상태입니다.

---
+++ src/config/constants/itemTypes.js
@@ -0,0 +1,6 @@
+const ITEM_TYPES = {
+  "p:sp": "text",
+  "p:pic": "image",
+};
+
+module.exports = ITEM_TYPES;
---
+++ src/utils/pptxParser.js
@@ -0,0 +1,219 @@
+import JSZip from "jszip";
+import PRESET_COLORS from "../config/constants/presetColors";
+import ITEM_TYPES from "../config/constants/itemTypes";
+
+const parser = new DOMParser();
+const EMUFactor = 96 / 914400;
+
+const getSlideSize = async (pptx) => {
+  const presentationTextXml = await pptx
+    .file("ppt/presentation.xml")
+    .async("text");
+  const slideSizeXml = parser
+    .parseFromString(presentationTextXml, "application/xml")
+    .querySelector("sldSz");
+  const {
+    cx: { nodeValue: slideXSize },
+    cy: { nodeValue: slideYSize },
+  } = slideSizeXml.attributes;
+
+  return {
+    slideWidth: slideXSize * EMUFactor,
+    slideHeight: slideYSize * EMUFactor,
+  };
+};
+
+const getSlidePaths = async (pptx) => {
+  const contentTypes = await pptx.file("[Content_Types].xml").async("text");
+  const slideOverrides = parser
+    .parseFromString(contentTypes, "application/xml")
+    .querySelectorAll("Override");
+
+  return Array.from(slideOverrides)
+    .filter((el) =>
+      el.attributes.PartName.nodeValue.includes("/ppt/slides/slide"),
+    )
+    .map((el) => el.attributes.PartName.nodeValue.slice(1));
+};
+
+const getSlideId = async (slide) => {
+  const IdListXml = slide.querySelectorAll("cSld extLst");
+  const slideIdXml = Array.from(IdListXml).find(
+    ({ nodeName }) => nodeName === "p:extLst",
+  );
+  const creationId =
+    slideIdXml.querySelector("ext creationId").attributes.val.nodeValue;
+
+  return creationId;
+};
+
+const getItemId = (item) => {
+  const {
+    id: { nodeValue: itemId },
+    name: { nodeValue: itemName },
+  } = item.querySelector("cNvPr").attributes;
+
+  return {
+    itemId,
+    itemName,
+  };
+};
+
+const getItemSize = (item) => {
+  const {
+    cx: { nodeValue: widthSize },
+    cy: { nodeValue: heightSize },
+  } = item.querySelector("xfrm ext").attributes;
+
+  return {
+    width: widthSize * EMUFactor,
+    height: heightSize * EMUFactor,
+  };
+};
+
+const getItemPosition = (item) => {
+  const {
+    x: { nodeValue: xSize },
+    y: { nodeValue: ySize },
+  } = item.querySelector("off").attributes;
+
+  return {
+    x: xSize * EMUFactor,
+    y: ySize * EMUFactor,
+  };
+};
+
+const getTextFontColor = (item) => {
+  const textAttributeXml = item.querySelector("txBody p r rPr");
+  const presetFontXml = textAttributeXml.querySelector("solidFill schemeClr");
+  const presetFontColor = presetFontXml?.attributes.val.nodeValue;
+  const presetFontDarkness =
+    textAttributeXml.querySelector("lumMod")?.attributes.val.nodeValue;
+
+  if (presetFontColor) {
+    const darkness = presetFontDarkness ? presetFontDarkness / 1000 : 0;
+    return PRESET_COLORS[presetFontColor][darkness];
+  }
+
+  const customFontColor =
+    textAttributeXml.querySelector("solidFill srgbClr")?.attributes.val;
+
+  return customFontColor ? customFontColor.nodeValue : "#000";
+};
+
+const getTextContent = (item) => {
+  const value = item.textContent;
+  const textAttributeXml = item.querySelector("txBody p r rPr");
+  const { b, i, u, sz } = textAttributeXml.attributes;
+  const backgroundColor =
+    textAttributeXml.querySelector("highlight srgbClr")?.attributes.val;
+  const fontColor = getTextFontColor(item);
+
+  return {
+    value,
+    isBold: !!b?.nodeValue,
+    isItalic: !!i?.nodeValue,
+    isUnderlined: !!u?.nodeValue,
+    fontColor,
+    size: sz ? (sz.nodeValue * 1) / 100 : 16,
+    backgroundColor: backgroundColor ? `#${backgroundColor.nodeValue}` : null,
+  };
+};
+
+const getImageContent = async (item, slidePath, pptx) => {
+  const embedId =
+    item.querySelector("blipFill blip").attributes["r:embed"].nodeValue;
+  const relsPath = `${slidePath.replace(
+    "ppt/slides/",
+    "ppt/slides/_rels/",
+  )}.rels`;
+  const relsXmlText = await pptx.file(relsPath).async("text");
+  const relsXmlDOM = parser
+    .parseFromString(relsXmlText, "application/xml")
+    .querySelector("Relationships").childNodes;
+  const embedIdRelXmlDOM = Array.from(relsXmlDOM).find(
+    (el) => el.attributes.Id.nodeValue === embedId,
+  );
+  const imagePath = embedIdRelXmlDOM.attributes.Target.nodeValue;
+  const imgSrc = await pptx
+    .file(imagePath.replace("..", "ppt"))
+    .async("base64");
+
+  return {
+    src: `data:image/*;base64,${imgSrc}`,
+  };
+};
+
+const getItemContent = async (item, itemType, slide, slidePath, pptx) => {
+  if (itemType === "text") {
+    return getTextContent(item);
+  }
+
+  if (itemType === "image") {
+    const imageContent = await getImageContent(item, slidePath, pptx);
+    return imageContent;
+  }
+
+  return {};
+};
+
+const getSlideItems = async (slide, slidePath, pptx) => {
+  const items = Array.from(
+    slide.querySelector("sld cSld spTree").children,
+  ).slice(2);
+
+  return Promise.all(
+    items.map(async (item, index) => {
+      const type = ITEM_TYPES[item.tagName] ?? "unidentified item";
+      const id = getItemId(item);
+      const { width, height } = getItemSize(item);
+      const { x, y } = getItemPosition(item);
+      const content = await getItemContent(item, type, slide, slidePath, pptx);
+
+      return {
+        type,
+        order: index + 1,
+        id,
+        width,
+        height,
+        x,
+        y,
+        content,
+      };
+    }),
+  );
+};
+
+const getSlides = async (pptx) => {
+  const slidePaths = await getSlidePaths(pptx);
+
+  return Promise.all(
+    slidePaths.map(async (path) => {
+      const slideTextXml = await pptx.file(path).async("text");
+      const slide = parser.parseFromString(slideTextXml, "application/xml");
+
+      const slideId = await getSlideId(slide);
+      const items = await getSlideItems(slide, path, pptx);
+
+      return {
+        slideId,
+        items,
+      };
+    }),
+  );
+};
+
+const pptxParser = async (pptxFile) => {
+  const pptx = await JSZip.loadAsync(pptxFile);
+
+  const { slideWidth, slideHeight } = await getSlideSize(pptx);
+  const slides = await getSlides(pptx);
+
+  return {
+    slideWidth,
+    slideHeight,
+    slides,
+  };
+};
+
+export default pptxParser;
