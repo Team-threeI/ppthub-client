d6984f094cb2dc42c577d05909cfb3d7fd2f051e
DonghyeonLee91 <donghyeonlee0908@gmail.com>
2022-10-18 14:39:17 +0900

    Feat: 파일업로드
    1. pptx파일을 업로드하면 서버에 전송할 수 있는 요청을 구현했습니다
    2. axios를 이용하여 formdata로 보내게 하였습니다
    3. input창은 accept= .pptx 를 적용하여 .pptx 파일만 들어오게 하였습니다
    4. packge.json에 프록시를 입력하여 cors에러를 회피했습니다

--- src/components/SlideViewer/FileAttachment.js
+++ src/components/SlideViewer/FileAttachment.js
@@ -1,11 +1,27 @@
 import React from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 function FileAttachment() {
+  const uploadFile = async (event) => {
+    const formData = new FormData();
+    formData.append("pptx", event.target.files[0]);
+
+    const res = await axios.post("/api/parse", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  };
+
   return (
     <FileInputLabel>
       <FileAddButton />
-      <FileInput type="file" />
+      <FileInput
+        type="file"
+        accept=".pptx"
+        onChange={(event) => uploadFile(event)}
+      />
     </FileInputLabel>
   );
 }
