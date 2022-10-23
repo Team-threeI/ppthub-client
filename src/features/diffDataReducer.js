f7580dae274b5203c8408045444bdf9ec5c155ce
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2022-10-23 21:01:05 +0900

    Setting: Redux diffDataReducer 추가 및 reducer 수정 - diffDataReducer를 추가하였습니다. - diffData를 다루게 되며, 이 후 diffData 시각화에 반영될 예정입니다. - diffData를 등록하는 액션(initializeDiffData), item 및 slide를 체크하는 액션을 생성 후 로직 작성하였습니다.

--- src/features/diffDataReducer.js
+++ src/features/diffDataReducer.js
@@ -1,19 +1,56 @@
 import { createAction, createReducer } from "@reduxjs/toolkit";
-import DIFFING_TYPES from "../config/constants/diffingTypes";
+import DIFF_TYPES from "../config/constants/diffingTypes";
 
-export const originalDiffData = createAction("originalDiffData");
+export const initializeDiffData = createAction("initializeDiffData");
+export const checkSlide = createAction("checkSlide");
+export const checkItem = createAction("checkItem");
 
-const initialState = {
-  [DIFFING_TYPES.DIFF_DATA]: {},
-};
+const initialState = {};
 
 const diffDataReducer = createReducer(initialState, {
-  [originalDiffData]: (state, action) => {
-    const { type, data } = action.payload;
+  [initializeDiffData]: (state, action) => {
+    return action.payload;
+  },
+  [checkSlide]: (state, action) => {
+    const slideId = action.payload;
+    const slideDiff = state[slideId].diff;
 
-    return Object.assign(state, {
-      [type]: { data },
-    });
+    if (slideDiff !== DIFF_TYPES.ADDED && slideDiff !== DIFF_TYPES.DELETED) {
+      return state;
+    }
+
+    return {
+      ...state,
+      [slideId]: {
+        ...state[slideId],
+        isChecked: !state[slideId].isChecked,
+      },
+    };
+  },
+  [checkItem]: (state, action) => {
+    const { slideId, itemId } = action.payload;
+    const itemDiff = state?.[slideId]?.items?.itemId?.diff ?? {};
+
+    if (
+      itemDiff !== DIFF_TYPES.MODIFIED &&
+      itemDiff !== DIFF_TYPES.ADDED &&
+      itemDiff !== DIFF_TYPES.DELETED
+    ) {
+      return state;
+    }
+
+    return {
+      ...state,
+      [slideId]: {
+        ...state[slideId],
+        items: {
+          [itemId]: {
+            ...state[slideId].items.itemId,
+            isChecked: !state[slideId].items.itemId.isChecked,
+          },
+        },
+      },
+    };
   },
 });
 
--- src/features/pptDataReducer.js
+++ src/features/pptDataReducer.js
@@ -6,17 +6,17 @@ export const registerData = createAction("registerData");
 export const deleteData = createAction("deleteData");
 
 const initialState = {
-  [PPT_DATA_TYPES.ORIGINAL_FILE]: {},
-  [PPT_DATA_TYPES.COMPARABLE_FILE]: {},
-  [PPT_DATA_TYPES.MERGED_FILE]: {},
+  [PPT_DATA_TYPES.ORIGINAL_PPT_DATA]: {},
+  [PPT_DATA_TYPES.COMPARABLE_PPT_DATA]: {},
+  [PPT_DATA_TYPES.MERGED_PPT_DATA]: {},
 };
 
 const pptDataReducer = createReducer(initialState, {
   [registerData]: (state, action) => {
-    const { type, data, dataId } = action.payload;
+    const { type, data, pptId } = action.payload;
 
     return Object.assign(state, {
-      [type]: { data, dataId },
+      [type]: { pptId, data },
     });
   },
   [deleteData]: (state, action) => {
@@ -25,16 +25,16 @@ const pptDataReducer = createReducer(initialState, {
     });
   },
   [changePreviousSequence]: (state, action) => {
-    if (Object.keys(state[PPT_DATA_TYPES.MERGED_FILE]).length) {
+    if (Object.keys(state[PPT_DATA_TYPES.MERGED_PPT_DATA]).length) {
       return state;
     }
-    if (Object.keys(state[PPT_DATA_TYPES.COMPARABLE_FILE]).length) {
+    if (Object.keys(state[PPT_DATA_TYPES.COMPARABLE_PPT_DATA]).length) {
       return Object.assign(state, {
-        [PPT_DATA_TYPES.COMPARABLE_FILE]: {},
-        [PPT_DATA_TYPES.MERGED_FILE]: {},
+        [PPT_DATA_TYPES.COMPARABLE_PPT_DATA]: {},
+        [PPT_DATA_TYPES.MERGED_PPT_DATA]: {},
       });
     }
-    if (Object.keys(state[PPT_DATA_TYPES.ORIGINAL_FILE]).length) {
+    if (Object.keys(state[PPT_DATA_TYPES.ORIGINAL_PPT_DATA]).length) {
       return Object.assign(state, initialState);
     }
 
